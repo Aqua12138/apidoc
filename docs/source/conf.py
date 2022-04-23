46a6d59920e0371b454ad058e2e0677decaf541d
Aqua12138 <88502156+Aqua12138@users.noreply.github.com>
2022-04-23 13:37:01 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,5 +1,10 @@
 # Configuration file for the Sphinx documentation builder.
-
+import os
+import sys
+import django
+sys.path.insert(0, os.path.abspath('../../'))
+os.environ['DJANGO_SETTINGS_MODULE'] = 'my_push_env.settings'
+django.setup()
 # -- Project information
 
 project = 'Lumache'
